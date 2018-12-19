9f909c859548c1d8bf21b390c4a225a42ab592ca
longhongjie <3601081@qq.com>
2018-12-19 21:17:23 +0800

    整体结构

--- inc/head.php
+++ inc/head.php
@@ -5,12 +5,10 @@ if($_SERVER['PHP_SELF']."" == "/OFPMS/inc/head.php" ) {
 }
 @session_start();
 ?>
-<table action="logout.php" width="100%">
-    <tr>
-        <td width="96%">欢迎，<?php echo $_SESSION['username']; ?></td>
-        <td width="4%"><a href="login.php?action=logout">退出登录</a></td>
-    </tr>
-    <tr>
-        <td height='2'></td>
-    </tr>
-</table>
+<div class="head">
+    <div class="left">&nbsp;</div>
+    <h1 class="mainTitle">有机农场管理系统</h1>
+    <div class="right">
+        <p>欢迎，<?php echo $_SESSION['username']; ?>&nbsp;&nbsp;<a href="login.php?action=logout">退出登录</a></p>
+    </div>
+</div>
---
+++ inc/menu.php
@@ -0,0 +1,21 @@
+<?php
+if($_SERVER['PHP_SELF']."" == "/OFPMS/inc/menu.php" ) {
+    //访问路径错误
+    header("Location: ../index.php");
+}
+// @session_start();
+?>
+<div class="menuList">
+    <ul>
+        <li><a href="#">土地管理</a></li>
+        <li><a href="#">生产材料购买</a></li>
+        <li><a href="#">配肥和配药管理</a></li>
+        <li><a href="#">生产计划管理</a></li>
+        <li><a href="#">农作记录管理</a></li>
+        <li><a href="#">实际采收管理</a></li>
+        <li><a href="#">提醒管理</a></li>
+        <li><a href="#">预计可采收量</a></li>
+        <li><a href="#">统计蔬菜经营盈亏</a></li>
+        <li><a href="#">生产经营折线图</a></li>
+    </ul>
+</div>
