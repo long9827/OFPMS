15403f42fbec6a6472c57c8fb08c4771d387a0b8
longhongjie <3601081@qq.com>
2018-12-21 12:07:09 +0800

    tmp

--- js/land.js
+++ js/land.js
@@ -1,53 +1,32 @@
 $(function() {
-    // $("#land_table").attr("align", "center");
-   
-    $("#showLand").click(function() {
-        loadland();
-    });
-    $("#remove").click(function() {
-        clear();
-    });
-    loadland();
+	pageinit();
 });
 
-//加载数据
-function loadland() {
-    var dataParam = {
-        region: $("#region").val(),
-        landmark: $("#landmark").val(),
-        block: $("#block").val(),
-        location: $("#location").val()
-    };
-    $.post("../src/queryLand.php",  dataParam, function(data, status) {
-        if(status=='success') {
-            data = JSON.parse(data);
-            if(data['code'] == 0) {
-                alert("服务器异常！");
-            } else {
-                newTable(data);
-            }
-        }
-    })
-}
-
-//构造table
-function newTable(data) {
-    clear();
-    var len = data['count'];
-    for(var i=0; i<len; ++i) {
-        var str = "<tr>"
-        +"<td>" + data['result'][i]['land_id'] + "</td>"
-        +"<td>" + data['result'][i]['region'] + "</td>"
-        +"<td>" + data['result'][i]['landmark'] + "</td>"
-        +"<td>" + data['result'][i]['block'] + "</td>"
-        +"<td>" + data['result'][i]['location'] + "</td>"
-        +"</tr>";
-        $("#land_table").append(str);
-    }
-    $("#land_table").length = len + 1;
-}
-
-//清空table
-function clear() {
-    $("#land_table tr").remove();
+function pageinit() {
+	jQuery("#jqGrid").jqGrid(
+			{
+				url : 'data/JSONData.json',//组件创建完成之后请求数据的url
+				datatype : "json",//请求数据返回的类型。可选json,xml,txt
+				colNames : [ 'Inv No', 'Date', 'Client', 'Amount', 'Tax','Total', 'Notes' ],//jqGrid的列显示名字
+				colModel : [ //jqGrid每一列的配置信息。包括名字，索引，宽度,对齐方式.....
+				             {name : 'id',index : 'id',width : 55}, 
+				             {name : 'invdate',index : 'invdate',width : 90}, 
+				             {name : 'name',index : 'name asc, invdate',width : 100}, 
+				             {name : 'amount',index : 'amount',width : 80,align : "right"}, 
+				             {name : 'tax',index : 'tax',width : 80,align : "right"}, 
+				             {name : 'total',index : 'total',width : 80,align : "right"}, 
+				             {name : 'note',index : 'note',width : 150,sortable : false} 
+				           ],
+				rowNum : 10,//一页显示多少条
+				rowList : [ 10, 20, 30 ],//可供用户选择一页显示多少条
+				pager : '#pager2',//表格页脚的占位符(一般是div)的id
+				sortname : 'id',//初始化的时候排序的字段
+				sortorder : "desc",//排序方式,可选desc,asc
+				mtype : "post",//向后台请求数据的ajax的类型。可选post,get
+				viewrecords : true,
+				caption : "JSON Example"//表格的标题名字
+			});
+	/*创建jqGrid的操作按钮容器*/
+	/*可以控制界面上增删改查的按钮是否显示*/
+	jQuery("#jqGrid").jqGrid('navGrid', '#pager2', {edit : false,add : false,del : false});
 }
