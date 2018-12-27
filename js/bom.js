268edb32cac43016aa4679a300c0edaa04c692a2
longhongjie <3601081@qq.com>
2018-12-27 15:45:10 +0800

    完成配肥配药管理

--- js/bom.js
+++ js/bom.js
@@ -10,6 +10,7 @@ function pageinit() {
             height: 600,
             scroll: false,
             uniqueId: "id",
+            detailView: true,
             columns: [{
                 field: 'id',
                 title: 'BOM_ID',
@@ -17,6 +18,9 @@ function pageinit() {
             }, {
                 field: 'number',
                 title: '编号'
+            }, {
+                field: 'bom_name',
+                title: '名称'
             }, {
                 field: 'produce_date',
                 title: '日期'
@@ -29,15 +33,317 @@ function pageinit() {
             },{
                 field: 'operate',
                 title: '操作',
-                width: 140,
+                width: 220,
                 align: 'center',
                 formatter: function(value, row, index) {
                     return [
-                        "<button type='button' class='btn btn-warning' onclick='edit(" + row.land_id + ")'>编辑</button>"
-                        + "&nbsp;<button type='button' class='btn btn-danger' onclick='deleteRow(" + row.land_id + ")'>删除</button>"
+                        "<button type='button' class='btn btn-info' onclick='add_material(" + row.id + ")'>添加原料</button>"
+                        + "&nbsp;<button type='button' class='btn btn-warning' onclick='edit(" + row.id + ")'>修改</button>"
+                        + "&nbsp;<button type='button' class='btn btn-danger' onclick='deleteRow(" + row.id + ")'>删除</button>"
                     ];
                 }
-            } ]
+            } ],
+            onExpandRow: function(index, row, $detail) {    //创建子表
+                var bom_id = row.id;
+                var sub = $detail.html("<table></table>").find('table');
+                $(sub).bootstrapTable({
+                    url : '../src/bom.php?action=list_detail',
+                    datatype: "json",
+                    method: 'post',
+                    contentType: 'application/x-www-form-urlencoded',
+                    queryParams: { bom_id: bom_id },
+                    uniqueId: "id",
+                    columns: [ {
+                        field: 'id',
+                        text: 'bom_detailID',
+                        visible: false
+                    }, {
+                        field: 'material_name',
+                        title: '原料名称'
+                    }, {
+                        field: 'material_wgt',
+                        title: '使用量'
+                    }, {
+                        field: 'unit',
+                        title: '单位'
+                    }, {
+                        field: 'operate',
+                        title: '操作',
+                        align: 'center',
+                        width: 150,
+                        formatter: function(value, row, index) {
+                            return [
+                                "<button type='button' class='btn btn-warning' onclick='editDetail(" + row.id + ")'>修改</button>"
+                                + "&nbsp;<button type='button' class='btn btn-danger' onclick='deleteDetail(" + row.id + ")'>删除</button>"
+                            ];
+                        }
+                    }],
+                    onClickRow: function(row, $element) {
+                        // vm.bom_detailInfo.material_name = row.material_name;
+                        // vm.bom_detailInfo.material_wgt = row.material_wgt;
+                        vm.bom_detailInfo = {
+                            id: row.id,
+                            material_name: row.material_name,
+                            material_wgt: row.material_wgt,
+                        };
+                        vm.subTable = sub;
+                        vm.rowID = bom_id;
+                    }
+                })
+            }
         });
-    console.log("ok");
 }
+
+//肥料或农药添加原料
+function add_material(id) {
+    vm.add_material(id);
+}
+
+//编辑肥料或农药信息
+function edit(id) {
+    vm.edit(id);
+}
+
+//删除肥料或农药信息
+function deleteRow(id) {
+    vm.delete(id);
+}
+
+//编辑原料
+function editDetail(id) {
+    vm.editDetail(id);
+}
+
+//删除原料
+function deleteDetail(id) {
+    //延迟10ms执行，等待vm.bom_detailInfo更新
+    setTimeout("vm.deleteDetail(" + id + ")", 10);
+}
+
+var vm = new Vue({
+	el: '#app',
+	data: {
+        addbomInfo: {},
+        bom_detailInfo: {},
+        subTable: null,
+        rowID: null,
+        bomInfo: {}
+	},
+	methods: {
+        search: function() {
+			$.ajax({
+				type: "post",
+				url: "../src/bom.php?action=list",
+				data: {
+                    number: vm.bomInfo.number,
+                    bom_name: vm.bomInfo.name,
+                    produce_date: vm.bomInfo.date
+				},
+				dataType:"json",
+				success : function(json) {
+					$("#table").bootstrapTable('load', json);
+				}
+			});
+		},
+        add: function() {
+            vm.addbomInfo = {},
+			$("#modal_title").text("新增");
+            $("#myModal").modal("show");
+        },
+        delete: function(id) {
+			var row = $("#table").bootstrapTable('getRowByUniqueId', id);
+			var msg = "确定要删除该记录：\n编号：" + row.number + "\n名称：" + row.bom_name; 
+			if(confirm(msg)) {
+				//确认删除记录
+				$.ajax({
+					type: "post",
+					url: "../src/bom.php?action=delete",
+					data: {
+						id: id,
+					},
+					dataType: "json",
+					success: function(json) {
+						if(json['code'] == '0') {
+							alert("删除成功");
+						} else {
+							console.log(json['msg']);
+							alert(json['msg']);
+						}
+						vm.reload();
+					}
+				});
+			}
+		},
+        edit: function(id) {
+            var row = $("#table").bootstrapTable('getRowByUniqueId', id);
+            vm.addbomInfo = {
+                id: id,
+                number: row.number,
+                name: row.bom_name,
+                date: row.produce_date,
+                cost: row.labor_cost,
+                waste_rate: row.waste_rate
+            };
+			$("#modal_title").text("修改");
+            $("#myModal").modal("show");
+        },
+        modalCommit: function() {
+			$("#myModal").modal("hide");
+			
+			if($("#modal_title").text() =="新增") {
+				//新增肥料或农药配料记录
+				$.ajax({
+					type: "post",
+					url: "../src/bom.php?action=add",
+					data: {
+                        number: vm.addbomInfo.number,
+                        produce_date: vm.addbomInfo.date,
+                        bom_name: vm.addbomInfo.name,
+                        labor_cost: vm.addbomInfo.cost,
+                        waste_rate: vm.addbomInfo.waste_rate
+					},
+					dataType: "json",
+					success: function(json) {
+						if(json['code'] == '0') {
+							alert("新增成功");
+						} else {
+							alert(
+                                json['msg']);
+						}
+						vm.reload();
+					}
+				});
+			} else if($("#modal_title").text() =="修改") {
+                //修改肥料或农药配料记录
+                console.log("xiugai");
+				$.ajax({
+					type: "post",
+					url: "../src/bom.php?action=edit",
+					data: {
+                        id: vm.addbomInfo.id,
+                        number: vm.addbomInfo.number,
+                        bom_name: vm.addbomInfo.name,
+                        produce_date: vm.addbomInfo.date,
+                        labor_cost: vm.addbomInfo.cost,
+                        waste_rate: vm.addbomInfo.waste_rate
+					},
+					dataType: "json",
+					success: function(json) {
+						if(json['code'] == '0') {
+							alert("修改成功");
+						} else {
+							alert(json['msg']);
+						}
+						vm.reload();
+					}
+				});
+			}
+        },
+        reload: function() {
+			$.ajax({
+				type: "post",
+				url: "../src/bom.php?action=list",
+				data: {},
+				dataType:"json",
+				success : function(json) {
+						   $("#table").bootstrapTable('load', json);
+				}
+			});
+        },
+        add_material: function(id) {
+            vm.bom_detailInfo = {
+                bom_id: id
+            },
+			$("#detailTitle").text("新增原料");
+            $("#detailModal").modal("show");
+        },
+        deleteDetail: function(id) {
+			var msg = "确定要删除原料：" + vm.bom_detailInfo.material_name;
+			if(confirm(msg)) {
+				//确认删除记录
+				$.ajax({
+					type: "post",
+					url: "../src/bom.php?action=deleteDetail",
+					data: {
+						id: id,
+					},
+					dataType: "json",
+					success: function(json) {
+						if(json['code'] == '0') {
+							alert("删除成功");
+						} else {
+							console.log(json['msg']);
+							alert(json['msg']);
+						}
+						vm.subReload();
+					}
+				});
+			}
+        },
+        editDetail: function(id) {
+            // console.log(vm.bom_detailInfo.material_name);
+            vm.bom_detailInfo.id = id;
+			$("#detailTitle").text("修改原料");
+            $("#detailModal").modal("show");
+        },
+        detailCommit: function() {
+            $("#detailModal").modal("hide");
+            if($("#detailTitle").text() =="新增原料") {
+                //新增原料
+                // console.log(vm.bom_id)
+				$.ajax({
+					type: "post",
+					url: "../src/bom.php?action=addDetail",
+					data: {
+                        bom_id: vm.rowID,
+                        material_name: vm.bom_detailInfo.material_name,
+                        material_wgt: vm.bom_detailInfo.material_wgt
+					},
+					dataType: "json",
+					success: function(json) {
+						if(json['code'] == '0') {
+							alert("新增成功");
+						} else {
+							alert(
+                                json['msg']);
+						}
+						vm.subReload();
+					}
+				});
+			} else if($("#detailTitle").text() =="修改原料") {
+                //修改原料
+				$.ajax({
+					type: "post",
+					url: "../src/bom.php?action=editDetail",
+					data: {
+                        id: vm.bom_detailInfo.id,
+                        material_name: vm.bom_detailInfo.material_name,
+                        material_wgt: vm.bom_detailInfo.material_wgt
+					},
+					dataType: "json",
+					success: function(json) {
+						if(json['code'] == '0') {
+							alert("修改成功");
+						} else {
+							alert(json['msg']);
+						}
+						vm.subReload();
+					}
+				});
+			}
+        },
+        subReload: function() {
+			$.ajax({
+				type: "post",
+				url: "../src/bom.php?action=list_detail",
+				data: {
+                    bom_id: vm.rowID
+                },
+				dataType:"json",
+				success : function(json) {
+						   $(vm.subTable).bootstrapTable('load', json);
+				}
+			});
+        }
+    }
+});
---
+++ src/bom.php
@@ -0,0 +1,147 @@
+<?php
+$action = $_GET['action'];
+// echo $action;
+
+if($action == "list") {
+    //查询配肥配药
+    $query = "select * from bom where 1=1  ";
+
+    if(isset($_POST['number'])) {
+        $number = $_POST['number'];
+        $query .= "and number like '%$number%' ";
+    }
+    if(isset($_POST['bom_name'])) {
+        $bom_name = $_POST['bom_name'];
+        $query .= "and bom_name like '%$bom_name%' ";
+    }
+    if(isset($_POST['produce_date'])) {
+        $produce_date = $_POST['produce_date'];
+        $query .= "and produce_date like '%$produce_date%' ";
+    }
+    $query .= "order by 'produce_date'";
+    // echo $query;
+    include("../inc/mysql.php");
+    $conn = new mysqlconn();
+    $rows = $conn->excu_json($query);
+    echo json_encode($rows);
+} elseif($action == "add") {
+    // 新增
+    if(isset($_POST['number']) && isset($_POST['produce_date']) && isset($_POST['bom_name']) 
+            && isset($_POST['labor_cost']) && isset($_POST['waste_rate'])) {
+        $number = $_POST['number'];
+        $produce_date = $_POST['produce_date'];
+        $bom_name = $_POST['bom_name'];
+        $labor_cost = $_POST['labor_cost'];
+        $waste_rate = $_POST['waste_rate'];
+        $sql = "INSERT INTO bom(number, produce_date, bom_name, labor_cost, waste_rate)
+            VALUES ('$number', '$produce_date', '$bom_name', '$labor_cost', '$waste_rate');";
+        // echo $query;
+        include("../inc/mysql.php");
+        $conn = new mysqlconn();
+        if($conn->excu($sql)) {
+            @$response->code = '0';
+        } else {
+            @$response->code = '-1';
+            @$response->msg = "添加失败！";
+        }
+    } else {
+        @$response->code = '-1';
+        @$response->msg = "请输入！";
+    }
+    echo json_encode($response);
+} elseif($action == "edit") {
+    //修改肥料或农药信息
+    $id = $_POST['id'];
+    $number = $_POST['number'];
+    $bom_name = $_POST['bom_name'];
+    $produce_date = $_POST['produce_date'];
+    $labor_cost = $_POST['labor_cost'];
+    $waste_rate = $_POST['waste_rate'];
+
+    $sql = "UPDATE bom SET number='$number', bom_name='$bom_name', 
+            produce_date='$produce_date', labor_cost='$labor_cost', waste_rate='$waste_rate'
+            WHERE id=$id";
+    // echo $sql;
+    include("../inc/mysql.php");
+    $conn = new mysqlconn();
+    if($conn->excu($sql)) {
+        @$response->code = '0';
+    } else {
+        @$response->code = '-1';
+        @$response->msg = "修该失败！";
+    }
+    echo json_encode($response);
+} elseif($action == "delete") {
+    //删除
+    $id = $_POST['id'];
+    // echo $id;
+    $sql_detail = "DELETE FROM bom_detail WHERE bom_id = $id";
+    $sql = "DELETE FROM bom WHERE id = $id";
+    // echo $sql_detail . " # " . $sql;
+    include("../inc/mysql.php");
+    $conn = new mysqlconn();
+    if($conn->excu($sql_detail) && $conn->excu($sql)) {
+        @$response->code = '0';
+    } else {
+        @$response->code = '-1';
+        @$response->msg = "删除失败！";
+    }
+    echo json_encode($response);
+
+} elseif($action == "list_detail") {
+    //查询肥料或农药原料
+    @$bom_id = $_POST['bom_id'];
+    $query = "select * from bom_detail where bom_id = $bom_id";
+    // echo $query;
+    include("../inc/mysql.php");
+    $conn = new mysqlconn();
+    $rows = $conn->excu_json($query);
+    echo json_encode($rows);
+} elseif($action == "addDetail") {
+    //增加原料
+    $bom_id = $_POST['bom_id'];
+    $material_name = $_POST['material_name'];
+    $material_wgt = $_POST['material_wgt'];
+    $sql = "INSERT INTO bom_detail(bom_id, material_name, material_wgt)
+            VALUES ($bom_id, '$material_name', $material_wgt);";
+     // echo $query;
+     include("../inc/mysql.php");
+     $conn = new mysqlconn();
+     if($conn->excu($sql)) {
+         @$response->code = '0';
+     } else {
+         @$response->code = '-1';
+         @$response->msg = "添加失败！";
+     }
+    echo json_encode($response);
+} elseif($action == "editDetail") {
+    //修改原料
+    $id = $_POST['id'];
+    $material_name = $_POST['material_name'];
+    $material_wgt = $_POST['material_wgt'];
+    $sql = "UPDATE bom_detail SET material_name = '$material_name', material_wgt = $material_wgt WHERE id = $id;";
+     // echo $query;
+     include("../inc/mysql.php");
+     $conn = new mysqlconn();
+     if($conn->excu($sql)) {
+         @$response->code = '0';
+     } else {
+         @$response->code = '-1';
+         @$response->msg = "修改失败！";
+     }
+    echo json_encode($response);
+} elseif($action == "deleteDetail") {
+    //删除原料
+    $id = $_POST['id'];    
+    $sql_detail = "DELETE FROM bom_detail WHERE id = $id";
+    include("../inc/mysql.php");
+    $conn = new mysqlconn();
+    if($conn->excu($sql_detail)) {
+        @$response->code = '0';
+    } else {
+        @$response->code = '-1';
+        @$response->msg = "删除失败！";
+    }
+    echo json_encode($response);
+}
+?>
